534ef2ea91eb02d5be9d3b5468778fba7150250d
邱晨龙 <601976246@qq.com>
2019-08-23 10:16:30 +0800

    Update SmsController.go

--- controllers/SmsController.go
+++ controllers/SmsController.go
@@ -23,12 +23,12 @@ type MessageResult struct {
 //Prepare 参考beego官方文档说明
 func (c *SmsController) Prepare() {
 	//先执行
-	c.BaseController.Prepare()
+	//c.BaseController.Prepare()
 	//如果一个Controller的多数Action都需要权限控制，则将验证放到Prepare
 	//c.checkAuthor("DataGrid", "DataList", "UpdateSeq", "UploadImage")
 	//如果一个Controller的所有Action都需要登录验证，则将验证放到Prepare
 	//权限控制里会进行登录验证，因此这里不用再作登录验证
-	c.checkLogin()
+	//c.checkLogin()
 }
 
 
